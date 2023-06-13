d3a79a3027f532090976f80044cf7e6eb3ba145f
barkatzx <barkatullah.zx@gmail.com>
2023-06-14 01:41:40 +0600

    user api update

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -10,23 +9,6 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-const verifyJWT = (req, res, next) => {
-  const authorization = req.headers.authorization;
-  if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' });
-  }
-  // bearer token
-  const token = authorization.split(' ')[1];
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ error: true, message: 'unauthorized access' })
-    }
-    req.decoded = decoded;
-    next();
-  })
-}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8fg5tj.mongodb.net/?retryWrites=true&w=majority`;
@@ -48,28 +30,15 @@ async function run() {
 
     const usersCollection = client.db('SummerDB').collection('users');
 
-    app.post('/jwt', (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
-
-      res.send({ token })
-    })
-
-
-    const verifyAdmin = async (req, res, next) => {
-      const email = req.decoded.email;
-      const query = { email: email }
-      const user = await usersCollection.findOne(query);
-      if (user?.role !== 'admin') {
-        return res.status(403).send({ error: true, message: 'forbidden message' });
-      }
-      next();
-    }
 
     app.post('/users', async (req, res) => {
       try {
-        const { name, email, photo } = req.body;
-        const user = { name, email, photo };
+        const { name, email } = req.body;
+        const user = { name, email };
+        const existingUser = await usersCollection.findOne(query);
+        if (existingUser) {
+          return res.send({ message: 'User Already Exists'})
+        }
         const result = await usersCollection.insertOne(user);
         res.status(201).json({ insertedId: result.insertedId });
       } catch (error) {
@@ -80,49 +49,11 @@ async function run() {
 
 
       // users related apis
-    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
-    // app.get('/users', async (req, res) => {
-    //   try {
-    //     const users = await usersCollection.find().toArray();
-    //     res.status(200).json({ users });
-    //   } catch (error) {
-    //     console.error('Failed to fetch users:', error);
-    //     res.status(500).json({ error: 'Failed to fetch users' });
-    //   }
-    // });
-
-
-    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
-      const email = req.params.email;
-
-      if (req.decoded.email !== email) {
-        res.send({ admin: false })
-      }
-
-      const query = { email: email }
-      const user = await usersCollection.findOne(query);
-      const result = { admin: user?.role === 'admin' }
-      res.send(result);
-    })
-
-    app.patch('/users/admin/:id', async (req, res) => {
-      const id = req.params.id;
-      console.log(id);
-      const filter = { _id: new ObjectId(id) };
-      const updateDoc = {
-        $set: {
-          role: 'admin'
-        },
-      };
-
-      const result = await usersCollection.updateOne(filter, updateDoc);
-      res.send(result);
-
-    })
 
     console.log("Connected to MongoDB successfully!");
   } catch (error) {
