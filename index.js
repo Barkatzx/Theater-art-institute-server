b4f37a46dc557759b6eabeb851eb38bbb104ddf2
barkatzx <barkatullah.zx@gmail.com>
2023-06-13 12:49:26 +0600

    JWT Added

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
-
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middlewear
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8fg5tj.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -22,43 +38,104 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server
     await client.connect();
-    const usersCollection = client.db("SummerDB").collection("users");
 
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        // const query = { email: user.email }
-        // const existingUser = await usersCollection.findOne(query);
+    const usersCollection = client.db('SummerDB').collection('users');
 
-        // if (existingUser) {
-        //     return res.send({ message: 'User already exits' })
-        // }
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
 
+      res.send({ token })
+    })
+
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
+    app.post('/users', async (req, res) => {
+      try {
+        const { name, email, photo } = req.body;
+        const user = { name, email, photo };
         const result = await usersCollection.insertOne(user);
-        res.send(result);
+        res.status(201).json({ insertedId: result.insertedId });
+      } catch (error) {
+        console.error('Failed to save user:', error);
+        res.status(500).json({ error: 'Failed to save user' });
+      }
     });
 
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+
+      // users related apis
+    app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // app.get('/users', async (req, res) => {
+    //   try {
+    //     const users = await usersCollection.find().toArray();
+    //     res.status(200).json({ users });
+    //   } catch (error) {
+    //     console.error('Failed to fetch users:', error);
+    //     res.status(500).json({ error: 'Failed to fetch users' });
+    //   }
+    // });
+
+
+    app.get('/users/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
+      }
+
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === 'admin' }
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+
+    console.log("Connected to MongoDB successfully!");
+  } catch (error) {
+    console.error('Failed to connect to MongoDB:', error);
   }
 }
+
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('Summer Camp Server Running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Summer Camp Server Running on port ${port}`);
-  })
+  res.send('Summer Camp Server Running');
+});
+
+app.listen(port, () => {
+  console.log(`Summer Camp Server Running on port ${port}`);
+});
