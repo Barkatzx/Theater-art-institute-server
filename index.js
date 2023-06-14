e05d29adf0192d268180d869cf721d82cd50c449
barkatzx <barkatullah.zx@gmail.com>
2023-06-14 15:40:18 +0600

    Verify Admin email

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 require('dotenv').config();
@@ -28,8 +27,7 @@ const verifyJWT = (req, res, next) => {
   })
 }
 
-
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l8fg5tj.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
@@ -56,8 +54,19 @@ async function run() {
       res.send({ token })
     })
 
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
      // users related apis
-     app.get('/users', async (req, res) => {
+     app.get('/users', verifyJWT, verifyAdmin, async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
@@ -114,17 +123,14 @@ async function run() {
     })
     
     
-    app.get('/users/admin/:email', async (req, res) => {
+    app.get('/users/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params.email;
-      if (req.decoded.email !== email) {
-        res.send({ admin: false })
-      }
-
       const query = { email: email }
       const user = await usersCollection.findOne(query);
       const result = { admin: user?.role === 'admin' }
       res.send(result);
     })
+
 
 
     console.log("Connected to MongoDB successfully!");
